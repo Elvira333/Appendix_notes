2633968ff51f52c02eab6314be5654f2dd9e118f
Elvira <elviraw@mail.ru>
2023-02-08 15:07:44 +0300

    Moved all the work from the note to the file "working with the note" added the method "save_as" and "open_file"

--- working_with_a_note.py
+++ working_with_a_note.py
@@ -1,10 +1,38 @@
-from main import form, text
+from tkinter import messagebox
+from tkinter.filedialog import asksaveasfile, askopenfile
 from tkinter import *
 
-file_name = NONE
+form = Tk()
+form.title("Заметки")
+form.geometry("500x500")
 
+menu_bar = Menu(form)
+file_menu = Menu(menu_bar)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+
+text = Text(form, width=500, height=500)
+
+file_name = NONE
 
 def new_file():
     global file_name
     file_name = "Без названия"
     text.delete('1.0', END)
+
+def save_as():
+    out = asksaveasfile(mode='w', defaultextension='.json')
+    data = text.get('1.0', END)
+    try:
+        out.write(data.rstrip())
+    except Exception:
+        messagebox.showerror("Ой", "Нельзя сохранить файл!")
+
+def open_file():
+    global file_name
+    inp = askopenfile(mode='r')
+    if inp is None:
+        return
+        file_name = inp.name
+    data = inp.read()
+    text.delete('1.0', END)
+    text.insert('1.0', data)
